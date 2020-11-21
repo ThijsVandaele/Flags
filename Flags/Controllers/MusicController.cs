7c6337f5d35e5b58fa6bf034d6446762ed93f194
Thijs Vandaele <thijs.vandaele@live.be>
2020-11-21 23:55:09 +0100

    Add music project

---
+++ Flags/Controllers/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using Flags.Models;
+using Flags.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Flags.Controllers
+{
+    public class MusicController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TrebleCleff()
+        {
+            return View();
+        }
+    }
+}
---
+++ Flags/wwwroot/js/Sketches/Music/TrebleScaleSketch.js
@@ -0,0 +1,50 @@
+﻿var firstLineY,
+  secondLineY,
+  thirdLineY,
+  FourthLineY,
+  FifthLineY;
+var spaceBetweenLines = 20;
+var noteHeight = spaceBetweenLines;
+var scaleLength = 200;
+var startScaleX = 100;
+var endOfScaleX = startScaleX + scaleLength;
+
+function setup() {
+  createCanvas(720, 400);
+  background(255);
+
+  firstLineY = 300;
+  secondLineY = firstLineY - spaceBetweenLines;
+  thirdLineY = secondLineY - spaceBetweenLines;
+  FourthLineY = thirdLineY - spaceBetweenLines;
+  FifthLineY = FourthLineY - spaceBetweenLines;
+}
+
+function draw() {
+  drawNote();
+  drawScale();
+}
+
+function drawScale() {
+  strokeWeight(1);
+  line(startScaleX, firstLineY, startScaleX + scaleLength, firstLineY);
+  line(startScaleX, secondLineY, startScaleX + scaleLength, secondLineY);
+  line(startScaleX, thirdLineY, startScaleX + scaleLength, thirdLineY);
+  line(startScaleX, FourthLineY, startScaleX + scaleLength, FourthLineY);
+  line(startScaleX, FifthLineY, startScaleX + scaleLength, FifthLineY);
+
+  strokeWeight(3);
+  line(endOfScaleX, firstLineY - 1, endOfScaleX, FifthLineY + 1);
+
+  strokeWeight(2);
+  line(endOfScaleX - 5, firstLineY, endOfScaleX - 5, FifthLineY);
+}
+
+function drawNote(){
+  var nX = startScaleX + (scaleLength / 2 );
+  var nY = thirdLineY - (spaceBetweenLines / 2);
+
+  strokeWeight(3);
+  ellipse(nX, nY, noteHeight * 1.2, noteHeight * .9);
+  
+}
